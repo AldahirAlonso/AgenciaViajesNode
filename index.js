081e8b16af1c1389d0af382c9cad08932b32fb9a
Aldahir <aldahir0603@hotmail.com>
2025-12-18 17:52:06 -0600

    Creando las vistas.

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ app.set('view engine', 'pug'); // Establecer el motor de plantillas PUG.
 
 // Obtener el año actual para pasarlo a las vistas.
 app.use( (req, res, next) => { // Middleware para obtener el año actual.
-//res.locals.unaVariable = 'Una Nueva Variable'; // Variable disponible en todas las vistas PUG. // locals es un objeto que contiene variables disponibles para la vista.
+//res.locals.unaVariable = 'Una Nueva Variable'; // Variable disponible en todas las vistas PUG. 
+                                                // locals es un objeto que contiene variables disponibles para la vista.
 //console.log(res.locals) // Ver las variables locales disponibles en la respuesta (en este caso, 'unaVariable').
     const year = new Date(); // Date es una clase de JavaScript para manejar fechas.
     res.locals.actualYear = year.getFullYear(); // getFullYear() obtiene el año completo (4 dígitos).
+    res.locals.nombreSitio = "Agencia de Viajes"; // Otra variable local para el nombre del sitio.
     next(); // Pasar al siguiente middleware o ruta.
 });
 
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,33 @@ import express from 'express';
 const router = express.Router();
 
 // Definir las rutas.
+// Una ruta es una URL que el usuario puede visitar en el navegador.
 // Ruta de Inicio.
 router.get('/', (req, res) => { // req - Lo que enviamos | res - Lo que express nos responde.
-    res.render('inicio')
+    res.render('inicio', { // Render sirve para renderizar una vista PUG.
+        pagina: 'Inicio' // Creamos la variable 'pagina' para pasar a la vista PUG.
+    })
 });
 
 // Ruta de Nosotros.
 router.get('/nosotros', (req, res) => { // req - Lo que enviamos | res - Lo que express nos responde.
-    res.render('nosotros')
+    res.render('nosotros', { // Render sirve para renderizar una vista PUG.
+        pagina: 'Nosotros' // Creamos la variable 'pagina' para pasar a la vista PUG.
+    })
+});
+
+// Ruta de Viajes.
+router.get('/viajes', (req, res) => { // req - Lo que enviamos | res - Lo que express nos responde.
+    res.render('viajes', { // Render sirve para renderizar una vista PUG.
+        pagina: 'Viajes' // Creamos la variable 'pagina' para pasar a la vista PUG.
+    })
+});
+
+// Ruta de Testimoniales.
+router.get('/testimoniales', (req, res) => { // req - Lo que enviamos | res - Lo que express nos responde.
+    res.render('testimoniales', { // Render sirve para renderizar una vista PUG.
+        pagina: 'Testimoniales' // Creamos la variable 'pagina' para pasar a la vista PUG.
+    })
 });
 
 // Exportar el router.
